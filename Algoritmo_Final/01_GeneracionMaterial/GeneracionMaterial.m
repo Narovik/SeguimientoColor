10e80faf627f04bfe5f707838519bfcf62123895
victornarov <victornarov@gmail.com>
2020-04-01 14:39:15 +0200

    Captura del vídeo

--- Algoritmo_Final/01_GeneracionMaterial/GeneracionMaterial.m
+++ Algoritmo_Final/01_GeneracionMaterial/GeneracionMaterial.m
@@ -1,3 +1,4 @@
+%% Captura del vídeo
 % Configuración dispositivo de captura
 video=videoinput('winvideo',1,'RGB24_640x480'); %30 fps
 video.ReturnedColorSpace = 'rgb';
@@ -13,3 +14,24 @@ video.FrameGrabInterval=3; %30/3=10fps
 % Configuración video de salida
 outvideo = VideoWriter('01_ColorNaranja.avi');
 outvideo.FrameRate = 10; %10 fps video de salida
+
+%Grabación del video
+open(outvideo);
+start(video);
+while(video.FramesAcquired < 150) %15s de grabacion
+    I=getdata(video,1);         %Obtenemos el frame capturado  
+    writeVideo(outvideo, I);    %Guardamos el frame    
+end
+
+stop(video);
+close(outvideo);
+
+%%Generación del conjunto de datos 
+clear
+video = VideoReader('01_ColorNaranja.avi');
+
+imagenes_naranja=[];
+
+
+
+
